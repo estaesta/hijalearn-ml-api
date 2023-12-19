d13d35e23e140561bc2960367d74a0842d33f470
estaesta <ahmadforesta@gmail.com>
2023-12-19 17:24:57 +0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import os
+from utils.prediction import inference
 
 from utils.preprocess import process_audio_to_spectrogram
 
@@ -7,7 +8,18 @@ app = Flask(__name__)
 
 model_list = [
     './model/model_full_inception_90.tflite',
-    './model/model_polos_inception_98.tflite'
+    './model/model_polos_inception_98.tflite',
+    './model/model_fathah_inception_93_2.tflite',
+    './model/model_kasrah_inception_86.tflite',
+    './model/model_dammah_inception_88.tflite'
+]
+
+label_csv_list = [
+    "./labels_csv/full_labels.csv",
+    "./labels_csv/polos_labels.csv",
+    "./labels_csv/fathah_labels.csv",
+    "./labels_csv/kasrah_labels.csv",
+    "./labels_csv/dhammah_labels.csv",
 ]
 
 @app.route("/")
@@ -35,15 +47,26 @@ def predict():
     # ku edit dikit dari kode anak ml biar gk usah import tensorflow
     preprocessed_audio_file = process_audio_to_spectrogram(audio_file)
 
+    # override model
+    # model = "0"
+
     # predict the audio file (use tflite model)
-    # TODO
-    prediction = "alif"
+    labels_csv = label_csv_list[int(model)]
+    model_path = model_list[int(model)]
+    prediction, probability = inference(preprocessed_audio_file, model_path, labels_csv)
+
+    probability = probability[0] * 100
+    # to string
+    probability = str(probability)
+
+    # prediction = "alif"
 
     file_name = audio_file.filename
     print("The file name is: {}".format(file_name))
 
     # return the prediction
-    return prediction
+    response = {"prediction": prediction, "probability": probability}
+    return jsonify(response)
 
 
 if __name__ == "__main__":
